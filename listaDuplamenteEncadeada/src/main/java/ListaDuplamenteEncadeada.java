60058045a4c89b1761a49bc97dcaf6e7f7b95aec
colacoxw <afae9000@gmail.com>
2023-11-14 13:59:37 -0300

    Criação do metodo add() na classe ListaDuplamenteEncadeada

--- listaDuplamenteEncadeada/src/main/java/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/src/main/java/ListaDuplamenteEncadeada.java
@@ -15,6 +15,33 @@ public class ListaDuplamenteEncadeada<T> {
     this.tamanhoLista = 0;
   }
 
+  public void add(T conteudo) {
+    NoDuplo<T> novoNo = new NoDuplo<>(conteudo);
+    novoNo.setNoProximo(null);
+    novoNo.setNoPrevio(ultimoNo);
+    if(primeiroNo == null) {
+      primeiroNo = novoNo;
+    }
+    if(ultimoNo != null){
+      ultimoNo.setNoProximo(novoNo);
+    }
+    ultimoNo = novoNo;
+  }
+
+  public T get(int index) {
+    return this.getNo(index).getConteudo();
+  }
+
+  private NoDuplo<T> getNo(int index) {
+    NoDuplo<T> noAuxiliar = primeiroNo;
+
+    for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+      noAuxiliar = noAuxiliar.getNoProximo();
+    }
+
+    return noAuxiliar;
+  }
+
   public int size() {
     return tamanhoLista;
   }
