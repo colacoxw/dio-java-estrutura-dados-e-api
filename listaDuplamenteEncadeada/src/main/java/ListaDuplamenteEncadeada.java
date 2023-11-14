9c427b982915e84042ffa0401464de105b0d074e
colacoxw <afae9000@gmail.com>
2023-11-14 14:34:14 -0300

    Criação do metodo remove() na classe ListaDuplamenteEncadeada

--- listaDuplamenteEncadeada/src/main/java/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/src/main/java/ListaDuplamenteEncadeada.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import main.java.one.digitalinnovation.No;
 import main.java.one.digitalinnovation.NoDuplo;
 
 public class ListaDuplamenteEncadeada<T> {
@@ -19,13 +20,58 @@ public class ListaDuplamenteEncadeada<T> {
     NoDuplo<T> novoNo = new NoDuplo<>(conteudo);
     novoNo.setNoProximo(null);
     novoNo.setNoPrevio(ultimoNo);
+
     if(primeiroNo == null) {
       primeiroNo = novoNo;
     }
+
     if(ultimoNo != null){
       ultimoNo.setNoProximo(novoNo);
     }
+
     ultimoNo = novoNo;
+    tamanhoLista++;
+  }
+
+  public void add(int index, T conteudo) {
+    NoDuplo<T> noAuxiliar = getNo(index);
+    NoDuplo<T> novoNo = new NoDuplo<>(conteudo);
+    novoNo.setNoProximo(noAuxiliar);
+
+    if(novoNo.getNoProximo() != null){
+      novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+      novoNo.getNoProximo().setNoPrevio(novoNo);
+    } else {
+      novoNo.setNoPrevio(ultimoNo);
+      ultimoNo = novoNo;
+    }
+
+    if(index == 0) {
+      primeiroNo = novoNo;
+    } else {
+      novoNo.getNoPrevio().setNoProximo(novoNo);
+    }
+
+    tamanhoLista++;
+  }
+
+  public void remove(int index) {
+    if(index == 0) {
+      primeiroNo = primeiroNo.getNoProximo();
+      if(primeiroNo != null) {
+        primeiroNo.setNoPrevio(null);
+      }
+    } else {
+      NoDuplo<T> noAuxiliar = getNo(index);
+      noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoPrevio());
+      if(noAuxiliar != ultimoNo){
+        noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());;
+      } else {
+        ultimoNo = noAuxiliar;
+      }
+    }
+    
+    this.tamanhoLista--;
   }
 
   public T get(int index) {
@@ -46,4 +92,18 @@ public class ListaDuplamenteEncadeada<T> {
     return tamanhoLista;
   }
 
+  @Override
+  public String toString() {
+    String strRetorno = "";
+
+    NoDuplo<T> noAuxiliar = primeiroNo;
+    for(int i = 0; i < size(); i++) {
+      strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}] ---> ";
+      noAuxiliar = noAuxiliar.getNoProximo();
+    }
+
+    strRetorno += "null";
+
+    return strRetorno;
+  }
 }
